d27bffb90b8deb6f6089e5edb25e9af97b7466e0
SalbaJnr <isaiahsalba2020@gmail.com>
2025-10-18 18:32:13 +0100

    Update vite-setup.ts

--- server/vite-setup.ts
+++ server/vite-setup.ts
@@ -42,7 +42,7 @@ export async function setupVite(app: Express, server: Server) {
     try {
       const clientTemplate = path.resolve(__dirname, "..", "client", "index.html");
 
-      // always reload the index.html file from disk incase it changes
+      // always reload the index.html file from disk in case it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
       template = template.replace(
         `src="/src/main.tsx"`,
