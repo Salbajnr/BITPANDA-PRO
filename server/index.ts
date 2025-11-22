b0a7759cb88d6b90973422014e2600c326f0cd54
Salbajnr <isaiahsalba2020@gmail.com>
2025-11-22 00:39:48 -0600

    `Added startServer() call and newline at end of file`

--- server/index.ts
+++ server/index.ts
@@ -509,3 +509,6 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('⚠️  Unhandled Rejection at:', promise, 'reason:', reason);
   // Application specific logging, throwing an error, or other logic here
 });
+
+// Start the server
+startServer();
